b319025028491fc7f859ffb91b9edc0ba169b6ee
kiranmai14 <krupakiranmai1122@gmail.com>
2021-10-14 12:34:40 +0530

    printing and getting the details of all files is finishef

--- Assignment 1/a.cpp
+++ Assignment 1/a.cpp
@@ -3,6 +3,15 @@
 #include <dirent.h>
 #include <vector>
 #include <algorithm>
+#include <ctime>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <cerrno>
+#include <cstring>
+#include <sstream>
+#include <string>
+#include <pwd.h>
+#include <grp.h>
 using namespace std;
 
 // to get the current working directory
@@ -12,9 +21,12 @@ char *get_cwd()
     getcwd(tmp, 256);
     return tmp;
 }
-//  sort the list of directories
-void sortFiles(struct dirent **diread, vector<char *> &files)
+//  get the list of directories and sort the list of directories
+vector<string> getAndSortFiles()
 {
+    vector<string> files;
+    DIR *dir;
+    struct dirent **diread;
     int n, i;
     n = scandir(".", &diread, 0, versionsort);
     if (n < 0)
@@ -24,39 +36,121 @@ void sortFiles(struct dirent **diread, vector<char *> &files)
         for (i = 0; i < n; ++i)
         {
             files.push_back(diread[i]->d_name);
-            // printf("%s\n", diread[i]->d_name);
-            // free(diread[i]);
+            free(diread[i]);
         }
-        // free(diread);
+        free(diread);
     }
+    return files;
 }
-bool getFiles(char *wd, vector<char *> &files)
+// get remissions in human readable form
+char *permissions(mode_t perm)
 {
-    DIR *dir;
-    struct dirent **diread;
-
-    // if ((dir = opendir(wd)) != nullptr)
-    // {
-    //     while ((*diread = readdir(dir)) != nullptr)
-    //     {
-    //         files.push_back((*diread)->d_name);
-    //     }
-    //     closedir(dir);
-    // }
-    // else
-    // {
-    //     perror("opendir");
-    //     return EXIT_FAILURE;
-    // }
-    sortFiles(diread,files);
-    return 0;
+    char *modeval = (char *)malloc(sizeof(char) * 10 + 1);
+    modeval[0] = (S_ISDIR(perm)) ? 'd' : '-';
+    modeval[1] = (perm & S_IRUSR) ? 'r' : '-';
+    modeval[2] = (perm & S_IWUSR) ? 'w' : '-';
+    modeval[3] = (perm & S_IXUSR) ? 'x' : '-';
+    modeval[4] = (perm & S_IRGRP) ? 'r' : '-';
+    modeval[5] = (perm & S_IWGRP) ? 'w' : '-';
+    modeval[6] = (perm & S_IXGRP) ? 'x' : '-';
+    modeval[7] = (perm & S_IROTH) ? 'r' : '-';
+    modeval[8] = (perm & S_IWOTH) ? 'w' : '-';
+    modeval[9] = (perm & S_IXOTH) ? 'x' : '-';
+    modeval[10] = '\0';
+    return modeval;
+}
+// get userid
+char *getUserId(uid_t st)
+{
+    struct passwd *pw = getpwuid(st);
+    if (pw != 0)
+        return pw->pw_name;
+    else
+        return nullptr;
+}
+char *getGroupId(gid_t st)
+{
+    struct group *gr = getgrgid(st);
+    if (gr != 0)
+        return gr->gr_name;
+    else
+        return nullptr;
+}
+// to get the size in human readable form
+string convertToString(double num)
+{
+    ostringstream convert;
+    convert << num;
+    return convert.str();
 }
 
+double roundOff(double n)
+{
+    double d = n * 100.0;
+    int i = d + 0.5;
+    d = (float)i / 100.0;
+    return d;
+}
+string convertSize(size_t size)
+{
+    static const char *SIZES[] = {"B", "KB", "MB", "GB"};
+    int div = 0;
+    size_t rem = 0;
+
+    while (size >= 1024 && div < (sizeof SIZES / sizeof *SIZES))
+    {
+        rem = (size % 1024);
+        div++;
+        size /= 1024;
+    }
+
+    double size_d = (float)size + (float)rem / 1024.0;
+    string result = convertToString(roundOff(size_d)) + " " + SIZES[div];
+    return result;
+}
+
+// to get details of the file
+bool getDetailsOfFiles(vector<string> files, vector<vector<string>> &filesWithdetails)
+{
+    struct stat fileInfo;
+    // for (auto file : files)
+    //     cout << file << endl;
+    for (int i = 0; i < files.size(); i++)
+    {
+        cout << files[i] << endl;
+        if (stat((const char *)files[i].c_str(), &fileInfo) != 0) // Use stat() to get the info
+        {
+            std::cerr << "Error: " << strerror(errno) << '\n';
+            return (EXIT_FAILURE);
+        }
+        string fileName = files[i];
+        vector<string> vect;
+        vect.push_back(fileName);
+        vect.push_back(convertSize(fileInfo.st_size));
+        vect.push_back(getUserId(fileInfo.st_uid));
+        vect.push_back(getGroupId(fileInfo.st_gid));
+        vect.push_back(permissions(fileInfo.st_mode));
+        vect.push_back(ctime(&fileInfo.st_mtime));
+        filesWithdetails.push_back(vect);
+    }
+    return true;
+}
 int main()
 {
     char *wd = get_cwd();
-    vector<char *> files; //directory files are stored in this variable
-    getFiles(wd,files);
-    for (auto file : files) cout << file << endl;
+    vector<string> files; // directory files names are stored in this variable
+    files = getAndSortFiles();
+    // for (auto file : files)
+    //     cout << file << endl;
+    vector<vector<string>> filesWithdetails;
+    getDetailsOfFiles(files, filesWithdetails);
+    for (auto file : filesWithdetails)
+    {
+        for (auto data : file)
+        {
+            cout << data << "\t";
+        }
+        cout << endl;
+    }
     return 0;
 }
